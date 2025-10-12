3dd5ef51cc5231a287ecd6ac47404a1b3c475a94
Natalie LAM <nlam@f1r3s8.clusters.42paris.fr>
2025-10-12 17:01:54 +0200

    FT(pt1) Add .shared directory

--- pt1/scripts/agent.sh
+++ pt1/scripts/agent.sh
@@ -4,8 +4,8 @@
 apt update && apt install -y curl
 
 # Export values (essential to install K3s as agent)
-export K3S_TOKEN_FILE=/vagrant_shared/token
 export K3S_URL=https://192.168.56.110:6443
+export K3S_TOKEN_FILE=/vagrant_shared/token
 
 # Install K3s (as agent)
 curl -sfL https://get.k3s.io | sh -
