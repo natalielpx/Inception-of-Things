5a9a98e2ee27fed6a6621622a41e2e3d201f9b78
natalie <natalielpx@gmail.com>
2026-02-02 23:19:33 +0100

    FIX (pt1 agent.sh) Read token with K3S_TOKEN
    Replaces reading token via K3S_TOKEN_FILE with K3S_TOKEN
    so token does not get deleted before being used for installation

--- pt1/scripts/agent.sh
+++ pt1/scripts/agent.sh
@@ -5,14 +5,9 @@ apt update && apt install -y curl
 
 # Export values (essential to install K3s as agent)
 export K3S_URL=https://192.168.56.110:6443
-export K3S_TOKEN_FILE=/vagrant_shared/token
-
-# Wait for token to be created and shared
-while ! [ -f /vagrant_shared/token ]
-do
-	echo "Waiting for token..."
-	sleep 5
-done
+export K3S_TOKEN=$(cat /vagrant_shared/token)
+# K3S_TOKEN_FILE: K3s deletes file after reading
+# K3S_TOKEN: K3s uses value directly
 
 # Install K3s (as agent) with forced IP configuration
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.111 --flannel-iface=eth1" sh -
