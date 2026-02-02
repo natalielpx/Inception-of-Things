8e6fbb57c52a31d8e33acbd7f78c3fcd73243e9b
natalie <natalielpx@gmail.com>
2026-02-02 21:52:51 +0100

    REF (pt1) Clean up unused code
    Removes waits in:
    - Vagrantfile
    - scripts/agent.sh
    - scripts/server.sh

--- pt1/scripts/agent.sh
+++ pt1/scripts/agent.sh
@@ -3,15 +3,16 @@
 # Install dependencies
 apt update && apt install -y curl
 
-# Wait until token exists and server responds
-until curl -k https://192.168.56.110:6443 >/dev/null 2>&1; do
-  echo "Waiting for K3s server to be ready..."
-  sleep 5
-done
-
 # Export values (essential to install K3s as agent)
 export K3S_URL=https://192.168.56.110:6443
 export K3S_TOKEN_FILE=/vagrant_shared/token
+
+# Wait for token to be created and shared
+while ! [ -f /vagrant_shared/token ]
+do
+	echo "Waiting for token..."
+	sleep 5
+done
 
 # Install K3s (as agent) with forced IP configuration
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.111 --flannel-iface=eth1" sh -
--- pt1/scripts/server.sh
+++ pt1/scripts/server.sh
@@ -6,10 +6,6 @@ apt update && apt install -y curl
 # Install K3s (as server) with forced IP configuration
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1" sh -
 
-
-# Wait a few seconds to ensure K3s is fully initialised
-sleep 10
-
 # Copy the generated token into the shared folder for the agent
 cp /var/lib/rancher/k3s/server/node-token /vagrant_shared/token
 chmod 644 /vagrant_shared/token
