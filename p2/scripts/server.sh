6c87a21622b8b506b66c816b3ce37ee667819e4f
natalie <natalielpx@gmail.com>
2026-02-10 17:54:41 +0100

    FT(p2) Add K3s server installation script
    After defining server machine
    Runs `scripts/server.sh` locally to install K3s as server

---
+++ p2/scripts/server.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+echo "================================================================"
+echo "RUNNING: server.sh"
+echo "Installs K3s as server on local machine"
+echo "================================================================"
+
+# Install dependencies
+echo "[1/2] Installing dependencies"
+apt update && apt install -y curl
+
+# Install K3s (as server) with forced IP configuration
+echo "[2/2] Installing K3s as server"
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1" sh -
+
+echo "================================================================"
+echo "COMPLETED: server.sh"
+echo "================================================================"
