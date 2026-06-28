87af85cf5e43fa56bc88d394cf7270310420a4f1
nlam <nlam@f1r4s4.paris.42.school>
2026-06-28 13:17:17 +0200

    FIX (os) Switches from ubuntu to alpine
    ubuntu requires more than 512 memory

--- p1/scripts/agent.sh
+++ p1/scripts/agent.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-apt update && apt install -y curl
+apk update && apk upgrade && apt add curl
 
 # Export values (essential to install K3s as agent)
 export K3S_URL=https://192.168.56.110:6443
--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-apt update && apt install -y curl
+apk update && apk upgrade && apt add curl
 
 # Install K3s (as server) with forced IP configuration
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1" sh -
