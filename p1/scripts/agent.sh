12b473aae7829077804ccb01c8f8f4080819dcfa
nlam <nlam@student.42.fr>
2026-07-26 16:20:47 +0200

    FT(p1) use ssh for k3s key

--- p1/scripts/agent.sh
+++ p1/scripts/agent.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-# Install dependencies
-apt update && apt install -y curl openssh-client
+echo "================================================================"
+echo "RUNNING: agent.sh"
+echo "Installs K3s as agent on local machine"
+echo "================================================================"
 
-# Export values (essential to install K3s as agent)
+echo "[1/4] Installing dependencies"
+apt-get update && apt-get install -y curl openssh-client
+
+echo "[2/3] Export essential values for K3s installation as agent"
 export K3S_URL=https://192.168.56.110:6443
-export K3S_TOKEN=$(ssh  -o StrictHostKeyChecking=no \
+export K3S_TOKEN=$(ssh  -o UserKnownHostsFile=/dev/null \
+                        -o StrictHostKeyChecking=no \
                         -o ConnectTimeout=5 \
                         -i "/home/vagrant/key.txt" \
                         "vagrant@192.168.56.110" \
@@ -13,8 +19,9 @@ export K3S_TOKEN=$(ssh  -o StrictHostKeyChecking=no \
 # K3S_TOKEN_FILE: K3s deletes file after reading
 # K3S_TOKEN: K3s uses value directly
 
-echo $K3S_URL
-echo $K3S_TOKEN
-
-# Install K3s (as agent) with forced IP configuration
+echo "[3/3] Install K3s as agent"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.111" sh -
+
+echo "================================================================"
+echo "COMPLETED: agent.sh"
+echo "================================================================"
--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
-# Install dependencies
-apt update && apt install -y curl
+echo "================================================================"
+echo "RUNNING: server.sh"
+echo "Installs K3s as server on local machine"
+echo "================================================================"
 
-# Install K3s (as server) with forced IP configuration
+echo "[1/4] Installing dependencies"
+apt-get update && apt-get install -y curl
+
+echo "[2/4] Install K3s as server"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110" sh -
 
-# Wait for token to generate
+echo "[3/4] Wait for token to generate"
 for i in {1..6};
 do
     if [ -f /var/lib/rancher/k3s/server/node-token ]; then
-        echo "Node token successfully generated"
+        echo "[4/4] Node token successfully generated"
         break
     fi
     echo "Waiting for node-token to be generated"
     sleep 30
 done
-
 if [ ! -f /var/lib/rancher/k3s/server/node-token ]; then
-    echo "Node token generation failed"
+    echo "[4/4]Node token generation failed"
     exit 1
 fi
+
+echo "================================================================"
+echo "COMPLETED: server.sh"
+echo "================================================================"
