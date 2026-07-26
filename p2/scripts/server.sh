3ffb2ade514412689f289e33aaf3685c445351e1
nlam <nlam@student.42.fr>
2026-07-26 18:33:55 +0200

    FIX(p2) switch from virtualbox to libvirt

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -6,11 +6,9 @@ echo "RUNNING: server.sh"
 echo "Installs K3s as server on local machine"
 echo "================================================================"
 
-# Install dependencies
 echo "[1/2] Installing dependencies"
-apt update && apt install -y curl
+apt-get update && apt-get install -y curl
 
-# Install K3s (as server) with forced IP configuration
 echo "[2/2] Installing K3s as server"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=192.168.56.110 --flannel-iface=eth1" sh -
 
